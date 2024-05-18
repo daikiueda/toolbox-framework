68c9b91e64a3e2233b575c6c554c27f5db5e5519
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2024-05-19 01:16:59 +0900

    Added prettier setting to skip destructive code actions on organizing imports

--- packages/__design-system/stories/Button.stories.js
+++ packages/__design-system/stories/Button.stories.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { fn } from '@storybook/test';
 import { Button } from './Button';
 
--- packages/__design-system/stories/Header.stories.js
+++ packages/__design-system/stories/Header.stories.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { fn } from '@storybook/test';
 import { Header } from './Header';
 
