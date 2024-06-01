a09ab239e569f299853eea652561d35b2245fc86
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2024-06-01 13:19:26 +0900

    Added TableViewUtil to @toolbox/design-system

--- packages/__design-system/Components/Collections/index.ts
+++ packages/__design-system/Components/Collections/index.ts
@@ -1,1 +1,32 @@
+import { useState } from 'react';
+
+import reverse from 'lodash/reverse';
+import sortBy from 'lodash/sortBy';
+
+import { SortDescriptor } from '@react-types/shared/src/collections';
+
 export { Cell, Column, Row, TableBody, TableHeader, TableView } from '@react-spectrum/table';
+
+const usePropsToSort = (initial: SortDescriptor = {}) => {
+  const [sortDescriptor, onSortChange] = useState<SortDescriptor>(initial);
+  return {
+    sortDescriptor,
+    onSortChange,
+  };
+};
+
+const toSorted = <T extends object>({
+  items,
+  sortDescriptor,
+}: {
+  items: T[];
+  sortDescriptor: SortDescriptor;
+}): T[] => {
+  const result = sortBy(items, sortDescriptor.column as string);
+  return sortDescriptor.direction === 'descending' ? reverse(result) : result;
+};
+
+export const TableViewUtil = {
+  usePropsToSort,
+  toSorted,
+};
