10ee1cc69dd410418e4cb942d0aa170d017adabc
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2024-06-04 01:42:25 +0900

    Added method to Toast to handle try...catch error

--- packages/__design-system/GlobalUI/Toast.ts
+++ packages/__design-system/GlobalUI/Toast.ts
@@ -1,1 +1,14 @@
-export { ToastContainer, ToastQueue as default } from '@react-spectrum/toast';
+export { ToastContainer } from '@react-spectrum/toast';
+import { ToastQueue } from '@react-spectrum/toast';
+
+export default {
+  ...ToastQueue,
+
+  // NOTE: try...catch receives content as any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  caughtError: (e: any, options: Parameters<typeof ToastQueue.negative>[1] = undefined) => {
+    console.error(e);
+    const children = e instanceof Error ? `${e.name}: ${e.message}` : (e as string);
+    return ToastQueue.negative(children, options);
+  },
+};
