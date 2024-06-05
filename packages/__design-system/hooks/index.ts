99cf2d16f2ac4b69980f28ddb8725b7f026ad917
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2024-06-05 13:46:02 +0900

    Reworked useSetting

--- packages/__design-system/hooks/index.ts
+++ packages/__design-system/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useAppEnv } from './useAppEnv';
 export { default as useClipboard } from './useClipboard';
+export { default as useSetting, type UpdateSetting } from './useSetting';
