d2cbd3f68b600c3aab17b197ded21686be4fc9d1
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2025-09-22 11:23:50 +0900

    refactor: デザインシステムのSortDescriptorを改善した
    独自に型定義していたことで利用側で調整が必要になっていたため、定義側を見直した

--- packages/__design-system/Components/Collections/index.ts
+++ packages/__design-system/Components/Collections/index.ts
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 
+import { SortDescriptor as _SortDescriptor } from '@react-types/shared/src/collections';
 import reverse from 'lodash/reverse';
 import sortBy from 'lodash/sortBy';
 
 export { Cell, Column, Row, TableBody, TableHeader, TableView } from '@react-spectrum/table';
 export type Selection = 'all' | Set<string | number>;
 
-// import { SortDescriptor } from "@react-types/shared/src/collections";
-export type SortDescriptor = {
-  column?: string | number;
-  direction?: 'ascending' | 'descending';
+export type SortDescriptor = _SortDescriptor;
+export const SortDescriptor = {
+  default: (): SortDescriptor => ({
+    column: 0,
+    direction: 'ascending',
+  }),
 };
 
-const usePropsToSort = (initial: SortDescriptor = {}) => {
+const usePropsToSort = (initial: SortDescriptor = SortDescriptor.default()) => {
   const [sortDescriptor, onSortChange] = useState<SortDescriptor>(initial);
   return {
     sortDescriptor,
