b73158aa37a17360da4ba21f5d5a8aafba61c69d
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2025-10-05 16:47:07 +0900

    chore: reorganized Radio storybook props

--- packages/__design-system/Components/Forms/Radio.stories.tsx
+++ packages/__design-system/Components/Forms/Radio.stories.tsx
@@ -5,6 +5,43 @@ import React from 'react';
 
 import { Radio, RadioGroup } from './index';
 
+const pets = [
+  { value: 'dogs', label: 'Dogs' },
+  { value: 'cats', label: 'Cats' },
+  { value: 'dragons', label: 'Dragons' },
+];
+
+const sizes = [
+  { value: 'small', label: 'Small' },
+  { value: 'medium', label: 'Medium' },
+  { value: 'large', label: 'Large' },
+];
+
+const officeSnacks = [
+  { value: 'fruit', label: 'Fruit' },
+  { value: 'chips', label: 'Chips' },
+  { value: 'nuts', label: 'Nuts' },
+];
+
+const notificationCadence = [
+  { value: 'instant', label: 'Instant' },
+  { value: 'daily', label: 'Daily digest' },
+  { value: 'weekly', label: 'Weekly summary' },
+];
+
+const renderRadios = (
+  args: React.ComponentProps<typeof RadioGroup>,
+  items: Array<{ value: string; label: string }>
+) => (
+  <RadioGroup {...args}>
+    {items.map(({ value, label }) => (
+      <Radio key={value} value={value}>
+        {label}
+      </Radio>
+    ))}
+  </RadioGroup>
+);
+
 /**
  * https://react-spectrum.adobe.com/react-spectrum/RadioGroup.html
  */
@@ -13,95 +50,119 @@ const meta: Meta<typeof RadioGroup> = {
   component: RadioGroup,
   tags: ['autodocs'],
   argTypes: {
-    orientation: { control: 'inline-radio', options: ['horizontal', 'vertical'] },
+    orientation: { control: { type: 'inline-radio' }, options: ['horizontal', 'vertical'] },
     isDisabled: { control: 'boolean' },
     isReadOnly: { control: 'boolean' },
     isRequired: { control: 'boolean' },
-    validationState: { control: 'inline-radio', options: ['valid', 'invalid'] },
+    isEmphasized: { control: 'boolean' },
+    showErrorIcon: { control: 'boolean' },
+    labelPosition: { control: { type: 'inline-radio' }, options: ['top', 'side'] },
+    labelAlign: { control: { type: 'inline-radio' }, options: ['start', 'end'] },
+    necessityIndicator: { control: { type: 'inline-radio' }, options: ['icon', 'label', 'none'] },
+    validationBehavior: { control: { type: 'inline-radio' }, options: ['aria', 'native'] },
+    isInvalid: { control: 'boolean' },
+    description: { control: 'text' },
+    errorMessage: { control: 'text' },
+    name: { control: 'text' },
     value: { control: 'text' },
+    defaultValue: { control: 'text' },
   },
   args: {
     label: 'Favorite pet',
+    orientation: 'vertical',
+    name: 'favorite-pet',
     onChange: fn(),
   },
 };
 export default meta;
 
-export const Basic: StoryObj<typeof RadioGroup> = {
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value="dogs">Dogs</Radio>
-      <Radio value="cats">Cats</Radio>
-      <Radio value="dragons">Dragons</Radio>
-    </RadioGroup>
-  ),
+type Story = StoryObj<typeof RadioGroup>;
+
+export const Basic: Story = {
+  render: (args) => renderRadios(args, pets),
 };
 
-export const Horizontal: StoryObj<typeof RadioGroup> = {
+export const Horizontal: Story = {
+  argTypes: {
+    orientation: { control: false },
+  },
   args: {
     orientation: 'horizontal',
   },
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value="small">Small</Radio>
-      <Radio value="medium">Medium</Radio>
-      <Radio value="large">Large</Radio>
-    </RadioGroup>
-  ),
+  render: (args) => renderRadios(args, sizes),
 };
 
-export const WithDefaultValue: StoryObj<typeof RadioGroup> = {
-  args: {
-    value: 'cats',
+export const SideLabel: Story = {
+  argTypes: {
+    labelPosition: { control: false },
   },
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value="dogs">Dogs</Radio>
-      <Radio value="cats">Cats</Radio>
-      <Radio value="dragons">Dragons</Radio>
-    </RadioGroup>
-  ),
+  args: {
+    label: 'Desk location',
+    labelPosition: 'side',
+    labelAlign: 'start',
+    description: 'Choose the desk closest to your teammates.',
+  },
+  render: (args) =>
+    renderRadios(
+      args,
+      ['A', 'B', 'C'].map((value) => ({ value, label: `Area ${value}` }))
+    ),
 };
 
-export const WithValidation: StoryObj<typeof RadioGroup> = {
+export const Required: Story = {
   args: {
+    label: 'Preferred office snack',
     isRequired: true,
-    validationState: 'invalid',
-    errorMessage: 'Please select an option',
+    necessityIndicator: 'label',
+    description: 'This helps us restock appropriately.',
   },
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value="option1">Option 1</Radio>
-      <Radio value="option2">Option 2</Radio>
-      <Radio value="option3">Option 3</Radio>
-    </RadioGroup>
-  ),
+  render: (args) => renderRadios(args, officeSnacks),
 };
 
-export const Disabled: StoryObj<typeof RadioGroup> = {
-  args: {
-    isDisabled: true,
-    value: 'option2',
+export const Invalid: Story = {
+  argTypes: {
+    isInvalid: { control: false },
   },
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value="option1">Option 1</Radio>
-      <Radio value="option2">Option 2</Radio>
-      <Radio value="option3">Option 3</Radio>
-    </RadioGroup>
-  ),
+  args: {
+    label: 'Team lunch option',
+    isInvalid: true,
+    errorMessage: 'Select one of the available options.',
+    showErrorIcon: true,
+  },
+  render: (args) =>
+    renderRadios(args, [
+      { value: 'pizza', label: 'Pizza' },
+      { value: 'sushi', label: 'Sushi' },
+      { value: 'salad', label: 'Salad' },
+    ]),
 };
 
-export const ReadOnly: StoryObj<typeof RadioGroup> = {
+export const Emphasized: Story = {
+  argTypes: {
+    isEmphasized: { control: false },
+  },
   args: {
+    label: 'Notification cadence',
+    isEmphasized: true,
+    description: 'Highlight important selection for new teammates.',
+    defaultValue: 'daily',
+  },
+  render: (args) => renderRadios(args, notificationCadence),
+};
+
+export const ReadOnly: Story = {
+  argTypes: {
+    isReadOnly: { control: false },
+  },
+  args: {
+    label: 'Assigned region',
     isReadOnly: true,
-    value: 'option2',
+    value: 'east',
   },
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value="option1">Option 1</Radio>
-      <Radio value="option2">Option 2</Radio>
-      <Radio value="option3">Option 3</Radio>
-    </RadioGroup>
-  ),
+  render: (args) =>
+    renderRadios(args, [
+      { value: 'north', label: 'North' },
+      { value: 'east', label: 'East' },
+      { value: 'west', label: 'West' },
+    ]),
 };
