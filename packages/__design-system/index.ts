33aa8b584dfc68604e52de6deae463b22f8faada
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2025-10-29 02:21:12 +0900

    feat(design-system): デザインシステムにBadgeを追加した

--- packages/__design-system/index.ts
+++ packages/__design-system/index.ts
@@ -5,6 +5,7 @@ export * from './Components/Overlays';
 export * from './Components/DropZone';
 export * from './Components/Forms';
 export * from './Components/Layout';
+export * from './Components/Status';
 
 export { default as Toast } from './GlobalUI/Toast';
 
