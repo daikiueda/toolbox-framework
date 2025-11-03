1c4668bdf142b5cc4c973814841098e09daed1f0
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2025-11-04 01:00:56 +0900

    feat(design-system): デザインシステムにProgressBarを追加した

--- packages/__design-system/Components/Status/index.ts
+++ packages/__design-system/Components/Status/index.ts
@@ -1,6 +1,7 @@
 export { Badge } from '@react-spectrum/badge';
 export { LabeledValue } from '@react-spectrum/labeledvalue';
 export { InlineAlert } from '@react-spectrum/inlinealert';
+export { ProgressBar } from '@react-spectrum/progress';
 
 export { default as InlineError } from './InlineError';
 export { default as AsyncLabeledValue } from './AsyncLabeledValue';
