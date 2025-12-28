ac9ffefa9346a0c02da7df0c78c516556b3faec4
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2025-12-29 00:39:02 +0900

    fix: electron.vite.config.tsをpackages/__electron/に移動した
    Electron以外の提供形態を考慮する将来に備えて

--- packages/__electron/electron.vite.config.ts
+++ packages/__electron/electron.vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
     build: {
       rollupOptions: {
         input: {
-          index: resolve(__dirname, 'packages/__electron/src/main/index.ts'),
+          index: resolve(__dirname, 'src/main/index.ts'),
         },
         external: ['@salesforce/core'],
       },
@@ -28,23 +28,23 @@ export default defineConfig(({ mode }) => ({
     build: {
       rollupOptions: {
         input: {
-          index: resolve(__dirname, 'packages/__electron/src/preload/index.ts'),
+          index: resolve(__dirname, 'src/preload/index.ts'),
         },
       },
     },
   },
   renderer: {
-    root: resolve(__dirname, 'packages/__electron/src/renderer/'),
+    root: resolve(__dirname, 'src/renderer/'),
     build: {
       rollupOptions: {
         input: {
-          index: resolve(__dirname, 'packages/__electron/src/renderer/index.html'),
+          index: resolve(__dirname, 'src/renderer/index.html'),
         },
       },
     },
     resolve: {
       alias: {
-        '@renderer': resolve(__dirname, 'packages/__electron/src/renderer/src'),
+        '@renderer': resolve(__dirname, 'src/renderer/src'),
       },
     },
     plugins: [react()],
