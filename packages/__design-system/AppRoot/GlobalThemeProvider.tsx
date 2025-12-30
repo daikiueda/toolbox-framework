538800a7a1f53ca81433bf789a567af37d976d61
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2025-12-31 00:59:38 +0900

    refactor(design-system): S2のProviderを共通のAppRootに追加した
    旧ProviderをS2版に置き換えるとエラーで画面が表示されなくなったので、コンポーネント側のマイグレーションが完了するまで新旧を併用する

--- packages/__design-system/AppRoot/GlobalThemeProvider.tsx
+++ packages/__design-system/AppRoot/GlobalThemeProvider.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Provider } from '@react-spectrum/provider';
+import { Provider as S2Provider } from '@react-spectrum/s2';
 import { theme } from '@react-spectrum/theme-default';
 
 import { type AppearanceMode, useAppearance } from '../hooks';
@@ -32,7 +33,7 @@ const GlobalThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
   return (
     <Provider theme={theme} colorScheme={mode}>
-      {children}
+      <S2Provider colorScheme={mode}>{children}</S2Provider>
     </Provider>
   );
 };
