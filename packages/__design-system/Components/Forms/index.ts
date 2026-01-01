92aef21b3a734f82385740d043666beb7ae89dad
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2026-01-02 00:27:39 +0900

    refactor(design-system): React Spectrumマイグレーション：Switch

--- packages/__design-system/Components/Forms/index.ts
+++ packages/__design-system/Components/Forms/index.ts
@@ -2,5 +2,4 @@ export { Form } from '@react-spectrum/s2';
 export { TextField, TextArea, NumberField } from '@react-spectrum/s2';
 export { Picker, PickerItem, PickerSection } from '@react-spectrum/s2';
 export { Radio, RadioGroup } from '@react-spectrum/s2';
-
-export { Switch } from '@react-spectrum/switch';
+export { Switch } from '@react-spectrum/s2';
