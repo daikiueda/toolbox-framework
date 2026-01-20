7b8a5c19b302f85ff77062228c5e8b5ddba61a6a
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2026-01-20 19:18:39 +0900

    feat(design-system): styleマクロ用の定数にネガティブマージンのための数値を追加した

--- packages/__design-system/style/Spece.ts
+++ packages/__design-system/style/Spece.ts
@@ -4,4 +4,12 @@ const Space = {
   Closest: 4,
 } as const;
 
+const NegativeSpace = {
+  OneLine: -28,
+  OneLetter: -16,
+  Closest: -4,
+} as const;
+
 export const space = (type: keyof typeof Space) => Space[type];
+
+export const negativeSpace = (type: keyof typeof Space) => NegativeSpace[type];
